3a1079460edf662b4ab3127aeb3d4088d6737c25
Tanguy Cizain <tanguycizain@gmail.com>
2021-06-14 10:44:33 +0200

    add heaptrack support

--- lib/system/alloc.nim
+++ lib/system/alloc.nim
@@ -740,6 +740,15 @@ when false:
       else:
         result = nil
 
+when defined(heaptracker):
+  const heaptrackLib =
+    when defined(heaptracker_inject):
+      "libheaptrack_inject.so"
+    else:
+      "libheaptrack_preload.so"
+  proc heaptrack_malloc(a: pointer, size: int) {.cdecl, importc, dynlib:heaptrackLib.}
+  proc heaptrack_free(a: pointer) {.cdecl, importc, dynlib:heaptrackLib.}
+
 proc rawAlloc(a: var MemRegion, requestedSize: int): pointer =
   when defined(nimTypeNames):
     inc(a.allocCounter)
@@ -812,6 +821,9 @@ proc rawAlloc(a: var MemRegion, requestedSize: int): pointer =
   sysAssert(allocInv(a), "rawAlloc: end")
   when logAlloc: cprintf("var pointer_%p = alloc(%ld)\n", result, requestedSize)
 
+  when defined(heaptracker):
+    heaptrack_malloc(result, requestedSize)
+
 proc rawAlloc0(a: var MemRegion, requestedSize: int): pointer =
   result = rawAlloc(a, requestedSize)
   zeroMem(result, requestedSize)
@@ -819,6 +831,8 @@ proc rawAlloc0(a: var MemRegion, requestedSize: int): pointer =
 proc rawDealloc(a: var MemRegion, p: pointer) =
   when defined(nimTypeNames):
     inc(a.deallocCounter)
+  when defined(heaptracker):
+    heaptrack_free(p)
   #sysAssert(isAllocatedPtr(a, p), "rawDealloc: no allocated pointer")
   sysAssert(allocInv(a), "rawDealloc: begin")
   var c = pageAddr(p)
